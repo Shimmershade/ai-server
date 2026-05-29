b39bb0fa762e3293f9cb67433f2e0de2f4cbf4fe
Тушь <106057273+Shimmershade@users.noreply.github.com>
2026-05-29 09:11:22 +0000

    save

--- test1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:8000/api/llm/ask \
-  -H "Content-Type: application/json" \
-  -d '{
-    "input": "тайны ордена тамплиеров",
-    "instructions": "Пиши на русском, кратко"
-  }'
